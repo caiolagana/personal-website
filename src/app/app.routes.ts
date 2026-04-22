0109b274d3ba9e031b86694fbcc1d9c965bc821d
Caio Laganá Fernandes <caiolagana@gmail.com>
2026-04-22 07:56:33 -0300

    alterando url para /music

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,5 +2,5 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   { path: '', loadComponent: () => import('./home/home').then(m => m.Home) },
-  { path: 'teoria-musical', loadComponent: () => import('./teoria-musical/teoria-musical').then(m => m.TeoriaMusical) },
+  { path: 'music', loadComponent: () => import('./teoria-musical/teoria-musical').then(m => m.TeoriaMusical) },
 ];
